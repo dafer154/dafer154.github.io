91f4f4495efacff086b6932182a7a47e7c337182
dafer154 <david_fza@hotmail.com>
2018-09-11 18:41:07 -0500

    Se crea el esqueleto de mi pagina web en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//Dependecies
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { MapComponent } from './shared/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    SkillComponent,
+    EducationComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
