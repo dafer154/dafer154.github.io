a0bac6ff89fd4ff72d337f79de649b55ebb37741
dafer154 <david_fza@hotmail.com>
2018-09-18 12:26:02 -0500

    Se arregla portafolio

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+
+  portfolioList = [
+    {
+      img : 'assets/images/portfolio/cier-sur.png',
+      project: 'Objetos virtuales de aprendizaje',
+      technologies: [
+        'Html5', 'Javascript', 'Jquery', 'CSS3, display(flex)', '@media'
+      ]
+    },
+    {
+      img: 'assets/images/portfolio/gimnasio.PNG',
+      project: 'Gimnasio de habilidades',
+      technologies: [
+        'Django rest framework', 'Python', 'React', 'Postgrest', 'CSS3, display(flex)', 'Html5'
+      ]
+    },
+    {
+      img : 'assets/images/portfolio/Cursos_estudiante.png',
+      project: 'Evaluacion formativa - EVAFORM',
+      technologies: [
+        'Python', 'Django-channels, Django-tenants', 'Postgrest', 'CSS3', 'Html5'
+      ]
+    }
+  ];
+
+
+
   constructor() { }
 
   ngOnInit() {
